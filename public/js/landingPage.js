6a35ebd450e8a634b7acd2297637923a66b47082
thatonkosi26 <thato2005nkosi@gmail.com>
2025-08-21 08:16:52 +0200

    First commit, nothing much going on, just setting up.

---
+++ public/js/landingPage.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBAJBUGWDylcBts8d8pNXH-4ru5ufC3kXY",
+  authDomain: "safe-drive-00.firebaseapp.com",
+  projectId: "safe-drive-00",
+  storageBucket: "safe-drive-00.firebasestorage.app",
+  messagingSenderId: "80416000393",
+  appId: "1:80416000393:web:4b7324d097c45c2bdaf958"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
